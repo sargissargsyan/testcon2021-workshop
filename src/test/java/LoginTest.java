7277b82b05decfeeaa085467f2b67ea504948bc6
Sargis Sargsyan <sargis.sargsyan@live.com>
2021-09-07 12:27:03 +0400

    login via API

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -5,10 +5,16 @@ import pages.LoginPage;
  * @author Sargis Sargsyan on 9/7/21
  * @project testcon2021
  */
-public class LoginTest {
+public class LoginTest extends SeleniumBase {
     @Test
     public void loginViaUI() {
         LoginPage loginPage = new LoginPage();
         loginPage.login("testcon2021@gmail.com", "Armenia2021");
     }
+
+    @Test
+    public void loginViaApi() {
+        login("testcon2021@gmail.com", "Armenia2021");
+    }
 }
+
---
+++ src/test/java/SeleniumBase.java
@@ -0,0 +1,26 @@
+import api.Client;
+import base.DriverHelper;
+import com.google.gson.JsonObject;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import pages.LoginPage;
+
+/**
+ * @author Sargis Sargsyan on 9/7/21
+ * @project testcon2021
+ */
+public class SeleniumBase {
+    private WebDriver driver = DriverHelper.get().getDriver();
+
+
+    public void login(String email, String password) {
+        JsonObject userJson = Client.login(email, password);
+        new LoginPage();
+        ((JavascriptExecutor) driver)
+                .executeScript("window.localStorage.setItem('token','" +
+                        userJson.get("auth_token") + "');");
+        ((JavascriptExecutor) driver)
+                .executeScript("window.localStorage.setItem('userInfo','" +
+                        userJson + "');");
+    }
+}
