cff19611b5643ae5105eac7dd8bb80e5b9ab3e44
Sargis Sargsyan <sargis.sargsyan@live.com>
2021-09-07 10:37:37 +0400

    driver creation

--- src/main/java/base/DriverHelper.java
+++ src/main/java/base/DriverHelper.java
@@ -1,7 +1,11 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Sargis Sargsyan on 9/7/21
@@ -10,8 +14,24 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class DriverHelper {
 
     public WebDriver driver;
-    private static ThreadLocal<WebDriver> driverThread  = new ThreadLocal<>();
+    private static ThreadLocal<WebDriver> driverThread = new ThreadLocal<>();
     private ChromeOptions options;
 
+    public WebDriver getDriver() {
+        if (driverThread.get() == null) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+            options = new ChromeOptions();
+            List<String> arguments = new ArrayList<>();
+            arguments.add("--disable-notifications");
+            options.addArguments(arguments);
+            driver = new ChromeDriver(options);
+            driverThread.set(driver);
+        }
+        return driverThread.get();
+    }
 
+    public void quitDriver(WebDriver driver) {
+        driver.quit();
+        driverThread.remove();
+    }
 }
